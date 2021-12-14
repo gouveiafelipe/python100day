7fbbb7fed1d7c6234ab6990c60d0c1f056951721
Felipe Gouveia <felipe.gouveiaa@gmail.com>
2021-12-14 19:48:50 -0300

    Project: tip calculator

--- day2/tipcalculator.py
+++ day2/tipcalculator.py
@@ -1,1 +1,17 @@
-print("push")
+# Project: tip calculator
+
+print("Welcome to the tip calculator!\n")
+
+total_bill = float(input("What was the total bill? $"))
+
+tip_percentage = int(input("What percentage tip would you like to give? 10, 12 or 15? "))
+
+people_split = int(input("How many people to split the bill? "))
+
+individual_bill = round((total_bill / people_split) * ((tip_percentage/100) + 1), 2)
+
+individual_bill = "{:.2f}".format(individual_bill)
+
+message = f"Each person should pay ${individual_bill}"
+
+print(message)
