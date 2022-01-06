598043e44a167e9d4b7d9898fa65f44f1de53a40
Felipe Gouveia <felipe.gouveiaa@gmail.com>
2022-01-06 20:31:59 -0300

    code improvements

--- day12/guessingname.py
+++ day12/guessingname.py
@@ -1,22 +1,24 @@
 from art import logo
 import random
 
-# generate a random number between 1 a 100
 print(logo)
-
 print("I'm thinking of a number between 1 and 100")
-
-user_difficulty = input(f"Choose a difficulty. Type 'easy' or hard: ")
-
-random_number = random.randrange(0, 101)
 user_chances = 0
 
-if user_difficulty == 'easy':
-    user_chances = 10
-else:
-    user_chances = 5
 
-print(f"You have {user_chances} attempts remaining to guess the number")
+def difficulty():
+    global user_chances
+    user_difficulty = ''
+    while user_difficulty != 'easy' or user_difficulty != 'hard':
+        user_difficulty = str(input(f"Choose a difficulty. Type 'easy' or hard: "))
+        if user_difficulty == 'easy':
+            user_chances = 10
+            break
+        elif user_difficulty == 'hard':
+            user_chances = 5
+            break
+        else:
+            print("Oops! Type a valid difficulty, 'easy' or 'hard'.  Try again...")
 
 
 def compare(user, bot):
@@ -26,40 +28,29 @@ def compare(user, bot):
         print("Too low.")
 
 
-user_number = 0
-
-
-while user_number != random_number:
-    while True:
-        try:
-            user_number = int(input("Make a guess: "))
+def game():
+    global user_chances
+    print(f"You have {user_chances} attempts remaining to guess the number")
+    user_number = 0
+    random_number = random.randrange(0, 101)
+    while user_number != random_number:
+        while True:
+            try:
+                user_number = int(input("Make a guess: "))
+                break
+            except ValueError:
+                print("Oops!  That was no valid number.  Try again...")
+        if user_number == random_number:
+            print(f"You got it!. The answer was {random_number}.")
             break
-        except ValueError:
-            print("Oops!  That was no valid number.  Try again...")
-    if user_number == random_number:
-        print(f"You got it!. The answer was {random_number}.")
-        break
-    user_chances -= 1
-    compare(user_number, random_number)
-    print("Guess again.")
-    print(f"You have {user_chances} attempts remaining to guess the number. ")
-    if user_chances == 0:
-        print("You've run out of a guesses, you lose")
-        break
+        user_chances -= 1
+        if user_chances == 0:
+            print("You've run out of a guesses, you lose")
+            break
+        compare(user_number, random_number)
+        print("Guess again.")
+        print(f"You have {user_chances} attempts remaining to guess the number. ")
 
 
-
-
-
-
-
-
-
-
-
-
-# 2 types of difficulty, if easy = 10 chances, hard = 5 chances
-
-# if the user guess the wrong number, print the remaining chances and if the guessed number is higher or lowest of the random number
-
-# if the guesses number its equal to the random number, the user win
+difficulty()
+game()
